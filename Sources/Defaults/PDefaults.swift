e8f08b05c063f76c8577b9715ad43e7ffa1be243
uuneo <uuneo@qq.com>
2025-06-01 23:32:04 +0800

    add

--- Sources/Defaults/PDefaults.swift
+++ Sources/Defaults/PDefaults.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import SwiftUICore
 
 public enum PD {
 	private static var cancellables = Set<AnyCancellable>()
@@ -69,5 +70,55 @@ public enum PD {
 			.sink { _ in
 				callBack(Self[key])
 			}.store(in: &set)
+	}
+	
+	// 支持监听多个键的变化
+	public static func observe<Value: Codable>(_ key: Key<Value>, onChange: @escaping (Value) -> Void) -> AnyCancellable {
+		onChange(self[key])
+		return NotificationCenter.default.publisher(for: .init(key.name))
+			.compactMap { $0.object as? Value }
+			.sink(receiveValue: onChange)
+	}
+	
+}
+
+
+
+
+// MARK: - 属性包装器改进
+
+@MainActor
+@frozen
+@propertyWrapper
+public struct PDS<Value: Codable>: DynamicProperty {
+	private let key: PD.Key<Value>
+	@State private var value: Value
+	private var cancellable: AnyCancellable?
+	
+	public var wrappedValue: Value {
+		get { value }
+		nonmutating set {
+			value = newValue
+			PD[key] = newValue
+		}
+	}
+	
+	public var projectedValue: Binding<Value> {
+		Binding(
+			get: { wrappedValue },
+			set: { wrappedValue = $0 }
+		)
+	}
+	
+	public init(_ key: PD.Key<Value>) {
+		self.key = key
+		_value = State(initialValue: PD[key])
+		
+		// 修正：使用 [self] 捕获，确保在主线程更新状态
+		self.cancellable = PD.observe(key) { [self] newValue in
+			DispatchQueue.main.async {
+				value = newValue
+			}
+		}
 	}
 }
